b579806d1de294e68af3c144722a0ad13d5d04f4
Alex Seifert <alex@alexseifert>
2013-08-06 23:19:38 +0200

    Initial IMAP support

--- index.php
+++ index.php
@@ -14,47 +14,11 @@
 		<script src="resources/js/scripts.js" type="text/javascript" charset="utf-8"></script>
 	</head>
 	<body>
-		<?php
-			try {
-				$server = "";
-				$user = "";
-				$passwd = "";
-				
-				include("mail.config");
-				
-				$inbox = imap_open($server,$user,$passwd) or die("Could not open Mailbox - try again later!");
-				$message_count = imap_num_msg($inbox);
-				
-				echo "Emails: ".$message_count;
-				
-				$emails = imap_search($inbox,'ALL');
-				
-				if($emails) {
-					$output = '';
-					
-					rsort($emails);
-					
-					foreach($emails as $email_number) {
-						/* get information specific to this email */
-						$overview = imap_fetch_overview($inbox,$email_number,0);
-						$message = imap_fetchbody($inbox,$email_number,2);
-						
-						/* output the email header information */
-						$output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
-						$output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
-						$output.= '<span class="from">'.$overview[0]->from.'</span>';
-						$output.= '<span class="date">on '.$overview[0]->date.'</span>';
-						$output.= '</div>';
-					}
-					
-					echo $output;
-				} 
-				
-				imap_close($mbox);
-			}
-			catch (Exception $e) {
-				throw new Exception('Something went wrong', 0, $e);
-			}
-		?>
+		<div class="emails">
+			
+		</div>
+		<div class="count">
+			Emails: <span id="emailcount"></span>
+		</div>		
 	</body>
 </html>
